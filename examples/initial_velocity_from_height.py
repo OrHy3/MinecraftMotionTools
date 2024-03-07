3242eddf71a20e7e109d0ab1b0074f1e533d2abc
OrHy3 <orhy3@pm.me>
2024-03-07 22:23:31 +0100

    Updated examples

--- examples/initial_velocity_from_height.py
+++ examples/initial_velocity_from_height.py
@@ -8,4 +8,5 @@ if h <= 0:
     print('Maximum height must be greater than 0!')
     exit()
 
-print('Y axis\' velocity:', mmt.v0_from_max_height(h, a=0.05, d=0.01, after=False))
+solutions = mmt.v0_from_max_height(h, a=0.05, d=0.01, after=False)
+print(f'Y axis\' velocity is between {solutions[1][0]} and {solutions[1][1]}')
